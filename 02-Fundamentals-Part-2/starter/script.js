d8c7e5e37c36dc7de43ddf1f14cc1cbe8340af49
MuhammadElshowehy <muhammadgamalelshowehy@gmail.com>
2024-07-31 17:53:13 +0300

    functions

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// function declaration: //
+function one() {
+  console.log("one");
+}
+one();
+
+// function expression: //
+const two = function() {
+  console.log("two");
+}
+two();
+
+function testStrict() {
+  console.log("strict");
+}
+testStrict();
