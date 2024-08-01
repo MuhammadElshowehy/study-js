a616e007b3d0ba1636bf81396c0bf9601ceb2e9c
MuhammadElshowehy <muhammadgamalelshowehy@gmail.com>
2024-08-01 15:26:26 +0300

    obj and loop

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -7,12 +7,33 @@ function one() {
 one();
 
 // function expression: //
-const two = function() {
+const two = function () {
   console.log("two");
-}
+};
 two();
 
 function testStrict() {
   console.log("strict");
 }
 testStrict();
+// object //
+const muhammad = {
+  fName: "Muhammad",
+  lName: "Elshowehy",
+  birthYear: 1999,
+  job: "frontend developer",
+  calcAge: function () {
+    this.age = 2024 - Number(this.birthYear);
+    return this.age;
+  },
+};
+const test = "job";
+console.log(muhammad.fName);
+console.log(muhammad["lName"]);
+console.log(muhammad.test); // => undefined
+console.log(muhammad[test]);
+
+console.log(muhammad.calcAge());
+console.log(muhammad["calcAge"]());
+console.log(muhammad);
+// loop //
