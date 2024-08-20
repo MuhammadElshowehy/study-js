5c36221db8005994a16d8a004255799e12bf274a
MuhammadElshowehy <muhammadgamalelshowehy@gmail.com>
2024-08-20 21:55:46 +0300

    ...

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,9 @@
 'use strict';
+let section = document.getElementById('test');
+console.log(section.innerHTML);
+console.log(section.innerText);
+console.log(section.textContent);
+
+console.log(window);
+console.log(document.images);
+console.log(document.images[0]);
